2b1e50b3d2de5faaa507edb8cb64748b4ef5b89c
Kai Yen <kyen99@gmail.com>
2026-08-29 09:32:03 -0400

    Float GNOME Calculator

--- default/hypr/apps/system.lua
+++ default/hypr/apps/system.lua
@@ -30,6 +30,8 @@ o.window("org.omarchy.about", { size = { 920, 480 } })
 o.window("dev.tensaku.Tensaku", { float = true })
 o.window("dev.tensaku.Tensaku", { center = true })
 o.window("omacalc", { float = true })
+o.window("org.gnome.Calculator", { float = true })
+o.window("org.gnome.Calculator", { center = true })
 
 -- Fullscreen screensaver.
 o.window("org.omarchy.screensaver", { fullscreen = true })
